c75698ff82793be948c8cb3ace275a83c4ae3434
allen li <allenxiruili@gmail.com>
2021-01-24 18:09:04 +0000

    Finished the evensum homework question.

---
+++ Lesson-one/evensum.py
@@ -0,0 +1,7 @@
+# Write a program to calculate the sum of all even numbers from 1 to 100
+i = 2
+sum = 0
+while i <= 100:
+  sum = sum + i
+  i += 2
+print(sum)
---
+++ Lesson-one/fibonacci.py
@@ -0,0 +1,3 @@
+Improve the previous work to allow user to set the range of number, start number and end number
+Write a program to generate the Fibonacci series
+Build the game: Guess the number.
