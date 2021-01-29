edd46494c046691e5abb28900e6ecc9f48f4bba5
allen li <allenxiruili@gmail.com>
2021-01-29 03:23:57 +0000

    Made the guess the number game

--- Lesson-one/guessnumber.py
+++ Lesson-one/guessnumber.py
@@ -1,1 +1,13 @@
-
+import random
+a = int(input("The range is from: "))
+b = int(input("To: "))
+r = random.randint(a, b)
+c = int(input("Guess a number: "))
+while True:
+  if c == r:
+    print("Correct!")
+    break
+  elif c > r:
+    c = int(input("Too high. Guess again: "))
+  else:
+    c = int(input("Too low. Guess again: "))
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("Welcome to my greeting program!")
-name = input("What is your name? ")
-print("Hi", name, ", Nice to meet you!")
-input("Press enter to exit!")
+import random
+a = int(input("The range is from: "))
+b = int(input("To: "))
+r = random.randint(a, b)
+c = int(input("Guess a number"))
+while True:
+  if c == r:
+    print("Correct!")
+    break
+  elif c > r:
+    c = int(input("Too high. Guess again: "))
+  else:
+    c = int(input("Too low. Guess again: "))
