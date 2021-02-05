08201bc3c326eaaed9c7bff6c21809061e5be338
allen li <allenxiruili@gmail.com>
2021-02-05 20:46:17 +0000

    I made changes to the gameborder

--- Lesson-two/gameborder.py
+++ Lesson-two/gameborder.py
@@ -7,32 +7,24 @@ def window(stdscr):
   # Set-up Colour
   curses.start_color()
   curses.use_default_colors()
-  curses.init_pair(127, 127, -1) 
-  curses.init_pair(197, 197, -1)
-  purple = curses.color_pair(127)
-  
+  curses.init_pair(70, 70, -1) 
+  curses.init_pair(4, 4, -1)
+  purple = curses.color_pair(70)
+  red = curses.color_pair(4)
   while True:
-    
+
     userKey = stdscr.getch()
     letter = chr(userKey)
     for y in range(0, sh-1):
       stdscr.addstr(y, 0, letter, purple)
       stdscr.addstr(y, sw-1, letter, purple)
-    for x in range(0, sw):
+    for x in range(0, sw-1):
       stdscr.addstr(sh-1, x, letter, purple)
       stdscr.addstr(0, x, letter, purple)
-
-    stdscr.addstr(sh // 2, sw // 2, "Code: {0}, Character: {1}".format(userKey, letter))
+    message = "Character: {1}, ASCII Code: {0}".format(userKey, letter)
+    stdscr.addstr(sh // 2, sw // 2 - len(message) // 2, message, red)
+    
     if userKey == 27:
       break
-      
-curses.wrapper(window)
-
-
-
-
-
-  
- 
 
 curses.wrapper(window)
